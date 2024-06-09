bf45594dcacbd39fa44c1495fd033a2b473b1da7
Jason Robison <jrrobison1@hoodsen.me>
2024-06-09 08:16:30 -0600

    Add docstrings

--- writing_feature_extractor/features/writing_feature.py
+++ writing_feature_extractor/features/writing_feature.py
@@ -9,14 +9,17 @@ class WritingFeature(ABC):
 
     @abstractmethod
     def get_graph_y_ticks(self) -> list[int]:
+        """Used for graphing. Returns the y-ticks for the graph."""
         pass
 
     @abstractmethod
     def get_graph_y_tick_labels(self) -> list[str]:
+        """Used for graphing. The y-axis tick labels."""
         pass
 
     @abstractmethod
     def get_y_level_label(self) -> str:
+        """Used for graphing. The label for the y-axis."""
         pass
 
     @abstractmethod
@@ -25,24 +28,35 @@ class WritingFeature(ABC):
 
     @abstractmethod
     def get_pydantic_feature_type(self) -> type:
+        """The type of the feature, as a Pydantic type. Typically an Enum."""
         pass
 
     @abstractmethod
     def get_pydantic_docstring(self) -> str:
+        """The docstring for the Pydantic type. This is useful as it may be
+        passed to the LLM to guide structured output."""
         pass
 
     @abstractmethod
     def get_graph_colors(self) -> dict[str, str]:
+        """Used for graphing. If the graph mode of this feature is COLOR,
+        these colors are used to color the bars of the graph."""
         pass
 
     @abstractmethod
     def get_int_for_enum(self, typ: type) -> int:
+        """Get the integer equivalent for an enum. This is a workaround
+        for an odd issue with graphing in which using the integer value of
+        an enum, or using the enum value itself, resulted in the graph being
+        displayed out of order and incorrectly."""
         pass
 
     @abstractmethod
     def add_result(self, enum_value):
+        """For results collection. Add a result to the results list."""
         pass
 
     @abstractmethod
     def set_graph_mode(self, graph_mode: WritingFeatureGraphMode):
+        """Set the graph mode for this feature"""
         pass
